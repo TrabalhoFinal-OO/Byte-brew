6e2244197fad82364c9dd76a8196c1fc325b56dd
bcaueLPS <30941831+bcaueLPS@users.noreply.github.com>
2026-06-28 15:08:13 -0300

    ClienteStandard.java

--- src/cafeteria/modelo/ClienteStandard.java
+++ src/cafeteria/modelo/ClienteStandard.java
@@ -1,5 +1,13 @@
 package cafeteria.modelo;
 
-public class ClienteStandard {
+public class ClienteStandard extends Cliente {
+	
+	public ClienteStandard(String nome, String cpf) {
+		super(nome, cpf);
+	}
 
-}
+	@Override
+	public void adicionarXP(double valor) {
+		super.adicionarXP(valor);
+	}
+}	

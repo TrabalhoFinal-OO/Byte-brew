d9acab7e6c7fec673665137d743be37c554fbfc1
Danielle Pinheiro <168576403+sdaniverse@users.noreply.github.com>
2026-06-28 14:58:08 -0300

    feat: implementa classe comida

--- src/cafeteria/modelo/Comida.java
+++ src/cafeteria/modelo/Comida.java
@@ -1,5 +1,26 @@
 package cafeteria.modelo;
 
-public class Comida {
+public class Comida extends Produto {
+    private int tempoPreparoMinutos;
+    private boolean isVegano;
+    private boolean isSemGluten;
 
+    public Comida(String codigo, String nome, double precoBase, int quantidadeEstoque, int tempoPreparoMinutos, boolean isVegano, boolean isSemGluten) {
+        super(codigo, nome, precoBase, quantidadeEstoque);
+        this.tempoPreparoMinutos = tempoPreparoMinutos;
+        this.isVegano = isVegano;
+        this.isSemGluten = isSemGluten;
+    }
+
+    public int getTempoPreparoMinutos() {
+        return tempoPreparoMinutos;
+    }
+
+    public boolean getIsVegano() {
+        return isVegano;
+    }
+
+    public boolean getIsSemGluten() {
+        return isSemGluten;
+    }
 }

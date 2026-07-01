fc7e00a0c3c172eb8dbeb207afcd78803a8e2f98
Miranda <lucasssmira@gmail.com>
2026-07-01 13:31:45 -0300

    feat: atualiza classes de exceção

--- src/cafeteria/excecao/EstoqueInsuficienteException.java
+++ src/cafeteria/excecao/EstoqueInsuficienteException.java
@@ -1,6 +1,8 @@
 package cafeteria.excecao;
 
 public class EstoqueInsuficienteException extends Exception {
+	
+	private static final long serialVersionUID = 1L;
 
 	public EstoqueInsuficienteException(String mensagem) {
 		super(mensagem);

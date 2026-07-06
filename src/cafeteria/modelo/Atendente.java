d40dc03f07cd31b9a8fd57c9f46ea4e3d32a1e56
Miranda <lucasssmira@gmail.com>
2026-07-06 19:18:50 -0300

    feat: implementa classe Atendente

--- src/cafeteria/modelo/Atendente.java
+++ src/cafeteria/modelo/Atendente.java
@@ -1,5 +1,23 @@
 package cafeteria.modelo;
 
 public class Atendente {
+	private String nome;
+	private String matricula;
+	
+	public Atendente(String nome, String matricula){
+		this.setNome(nome);
+		this.matricula = matricula;
+	}
 
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getMatricula() {
+		return matricula;
+	}
 }

6210e680fbafba379722e5834602b02af1e1656b
thamiresellen <thamiresellen@localhost>
2026-07-06 11:35:57 -0300

    fix

--- src/cafeteria/app/Main.java
+++ src/cafeteria/app/Main.java
@@ -1,19 +1,21 @@
 package cafeteria.app;
 
-import cafeteria.excecao.EstoqueInsuficienteException;
-import cafeteria.excecao.PontosInsuficientesException;
-import cafeteria.modelo.*;
+import cafeteria.modelo.Atendente;
+import cafeteria.modelo.Bebida;
+import cafeteria.modelo.Comida;
+import cafeteria.modelo.Produto;
+import cafeteria.modelo.ProdutoRepository;
+import cafeteria.modelo.Pedido;
+import cafeteria.modelo.ItemPedido;
+import cafeteria.modelo.Cliente;
+import cafeteria.modelo.ClienteVIP;
+import cafeteria.modelo.ClienteStandard;
 
-public class Main{
+public class Main {
+
 	public static void main(String[] args) {
-		System.out.println("BEM-VINDO A CAFETERIA GEEK BYTE E BREW");
-		
-		//criacao do cardapio 
-		
-		//cadastro do programa de fidelidade 
-		
-		//fluxo de venda 
+		System.out.println("Bem-vindo ao sistema da Cafeteria Byte & Brew!");
 		
 		
 	}
-}
+	}
